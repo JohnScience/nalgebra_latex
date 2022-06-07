2f1bb04e567fee2aecd4c631c6122bcbe7a0eca8
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-06-06 18:21:02 -0600

    Defined matrix environments

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,67 @@ use core::fmt::{Write, Display, Error};
 
 use nalgebra::{Matrix, Dim, RawStorage};
 
+/// Implementers of the trait represent different [LaTeX] [environments].
+/// 
+/// # Notes
+/// 
+/// * *`<const ENV: &'static str>` is not a type parameter of the trait
+/// because at the moment of writing it is supported only with #![feature(adt_const_params)]*
+///
+/// [LaTeX]: https://www.overleaf.com/learn/latex/Learn_LaTeX_in_30_minutes#What_is_LaTeX.3F
+/// [environments]: https://www.overleaf.com/learn/latex/Environments
+pub trait LatexEnvironment {
+    /// Writes name of the [LaTeX] [environment] to the given destination that implements the [`Write`] trait.
+    /// 
+    /// # Arguments
+    /// 
+    /// `W` - type argument of the destination, expected to implement the [`Write`] trait.
+    /// `dest` - destination to write the name of the [LaTeX] [environment] to.
+    /// 
+    /// # Returns
+    /// 
+    /// [`Result`]`<(), `[`core::fmt::Error`]`>` - [`Result::Ok`] if the name of the [LaTeX]
+    /// [environment] was successfully written to the destination and [`Result::Err`] otherwise.
+    /// 
+    /// # Example
+    /// 
+    // TODO: add example
+    /// ```
+    /// ```
+    /// 
+    /// *Note: The name of the environment is supplied via a function instead of an associated constant
+    /// because the function can be implemented on static string like "smallpbBvVmatrix" that would
+    /// be shared between different matrix environments. Such implementation may or may not have lighter
+    /// memory footprint, better cache locality, and performance.*
+    /// 
+    /// [LaTeX]: https://www.overleaf.com/learn/latex/Learn_LaTeX_in_30_minutes#What_is_LaTeX.3F
+    /// [environment]: https://www.overleaf.com/learn/latex/Environments
+    fn write_name<W: Write>(dest: &mut W) -> Result<(), Error>;
+
+    fn write_begin<W: Write>(dest: &mut W) -> Result<(), Error> {
+        dest.write_str(r"\begin{")?;
+        Self::write_name(dest)?;
+        dest.write_str("}")
+    }
+    fn write_end<W: Write>(dest: &mut W) -> Result<(), Error> {
+        dest.write_str(r"\end{")?;
+        Self::write_name(dest)?;
+        dest.write_str("}")
+    }
+}
+
+pub struct PlainMatrixEnvironment;
+
+pub struct ParenthesizedMatrixEnvironment;
+
+pub struct BracketedMatrixEnvironment;
+
+pub struct BracedMatrixEnvironment;
+
+pub struct VBarDelimitedMatrixEnvironment;
+
+pub struct DoubleVBarDelimitedMatrixEnvironment;
+
 /// Implementers of the trait allow formatting of values of type `&I` in the form of [LaTeX] strings.
 /// 
 /// # Example
@@ -95,55 +156,6 @@ pub trait LatexFormatter<I> {
     fn write_latex<W: Write>(dest: &mut W, input: &I) -> Result<(), Error>;
 }
 
-/// Implementers of the trait represent different [LaTeX] [environments].
-/// 
-/// # Notes
-/// 
-/// * *`<const ENV: &'static str>` is not a type parameter of the trait
-/// because at the moment of writing it is supported only with #![feature(adt_const_params)]*
-///
-/// [LaTeX]: https://www.overleaf.com/learn/latex/Learn_LaTeX_in_30_minutes#What_is_LaTeX.3F
-/// [environments]: https://www.overleaf.com/learn/latex/Environments
-pub trait LatexEnvironment {
-    /// Writes name of the [LaTeX] [environment] to the given destination that implements the [`Write`] trait.
-    /// 
-    /// # Arguments
-    /// 
-    /// `W` - type argument of the destination, expected to implement the [`Write`] trait.
-    /// `dest` - destination to write the name of the [LaTeX] [environment] to.
-    /// 
-    /// # Returns
-    /// 
-    /// [`Result`]`<(), `[`core::fmt::Error`]`>` - [`Result::Ok`] if the name of the [LaTeX]
-    /// [environment] was successfully written to the destination and [`Result::Err`] otherwise.
-    /// 
-    /// # Example
-    /// 
-    // TODO: add example
-    /// ```
-    /// ```
-    /// 
-    /// *Note: The name of the environment is supplied via a function instead of an associated constant
-    /// because the function can be implemented on static string like "smallpbBvVmatrix" that would
-    /// be shared between different matrix environments. Such implementation may or may not have lighter
-    /// memory footprint, better cache locality, and performance.*
-    /// 
-    /// [LaTeX]: https://www.overleaf.com/learn/latex/Learn_LaTeX_in_30_minutes#What_is_LaTeX.3F
-    /// [environment]: https://www.overleaf.com/learn/latex/Environments
-    fn write_name<W: Write>(dest: &mut W) -> Result<(), Error>;
-
-    fn write_begin<W: Write>(dest: &mut W) -> Result<(), Error> {
-        dest.write_str(r"\begin{")?;
-        Self::write_name(dest)?;
-        dest.write_str("}")
-    }
-    fn write_end<W: Write>(dest: &mut W) -> Result<(), Error> {
-        dest.write_str(r"\end{")?;
-        Self::write_name(dest)?;
-        dest.write_str("}")
-    }
-}
-
 /// As implementor of [`LatexFormatter`]`<`[`nalgebra::Matrix`]`<_,_,_,_>>` trait, this type offers
 /// formatting matrices as default ["environment"]-agnostic [LaTeX] matrix contents.
 ///
@@ -167,6 +179,43 @@ pub trait LatexEnvironment {
 pub struct PlainMatrixContentsFormatter;
 
 pub struct PlainMatrixFormatter;
+
+impl LatexEnvironment for PlainMatrixEnvironment {
+    fn write_name<W: Write>(dest: &mut W) -> Result<(), Error> {
+        dest.write_str("matrix")
+    }
+}
+
+impl LatexEnvironment for ParenthesizedMatrixEnvironment {
+    fn write_name<W: Write>(dest: &mut W) -> Result<(), Error> {
+        dest.write_str("pmatrix")
+    }
+}
+
+impl LatexEnvironment for BracketedMatrixEnvironment {
+    fn write_name<W: Write>(dest: &mut W) -> Result<(), Error> {
+        dest.write_str("bmatrix")
+    }
+}
+
+impl LatexEnvironment for BracedMatrixEnvironment {
+    fn write_name<W: Write>(dest: &mut W) -> Result<(), Error> {
+        dest.write_str("Bmatrix")
+    }
+}
+
+impl LatexEnvironment for VBarDelimitedMatrixEnvironment {
+    fn write_name<W: Write>(dest: &mut W) -> Result<(), Error> {
+        dest.write_str("vmatrix")
+    }
+}
+
+impl LatexEnvironment for DoubleVBarDelimitedMatrixEnvironment {
+
+    fn write_name<W: Write>(dest: &mut W) -> Result<(), Error> {
+        dest.write_str("Vmatrix")
+    }
+}
 
 impl<T,R,C,S> LatexFormatter<Matrix<T,R,C,S>> for PlainMatrixContentsFormatter
 where
