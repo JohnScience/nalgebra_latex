c72c526fdea32d15f7ba12e00fd72734318ed374
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-06-25 13:37:10 -0600

    Prepared for publishing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![doc = include_str!("../README.md")]
 #![cfg_attr(not(any(doc, test, doctest, feature = "lin_sys")), no_std)]
 #![cfg_attr(feature = "adt_const_params", feature(adt_const_params))]
+#![cfg_attr(doc_cfg, feature(doc_cfg))]
 
 pub mod env;
 pub mod fmt;
