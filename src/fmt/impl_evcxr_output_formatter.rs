d318cfa6e720b2b0522e7691985f9c2ab8406b83
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-07-17 22:53:37 -0600

    Changed MIME supplied for evcxr

--- src/fmt/impl_evcxr_output_formatter.rs
+++ src/fmt/impl_evcxr_output_formatter.rs
@@ -1,9 +1,9 @@
 use crate::latex_modes::{DisplayMathMode, InnerParagraphMode};
-use mime_typed::{evcxr_support::TextMarkdownUtf8, MimeStrExt};
+use mime_typed::{evcxr_support::TextMarkdown, MimeStrExt};
 
 use super::{EvcxrOutputFormatter, LatexFormatter};
 
-impl<F, I> EvcxrOutputFormatter<TextMarkdownUtf8, I> for F
+impl<F, I> EvcxrOutputFormatter<TextMarkdown, I> for F
 where
     F: LatexFormatter<InnerParagraphMode, DisplayMathMode, I>,
 {
@@ -11,7 +11,7 @@ where
         dest: &mut W,
         input: &I,
     ) -> Result<(), core::fmt::Error> {
-        writeln!(dest, "EVCXR_BEGIN_CONTENT {}", TextMarkdownUtf8::MIME_STR)?;
+        writeln!(dest, "EVCXR_BEGIN_CONTENT {}", TextMarkdown::MIME_STR)?;
         F::write_latex(dest, input)?;
         dest.write_str("\nEVCXR_END_CONTENT\n")
     }
