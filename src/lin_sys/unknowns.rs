7fe12dd1717993d25e28df02ce6ae35e394d815f
JohnScience <demenev.dmitriy1@gmail.com>
2022-08-18 14:46:03 -0600

    Added a vector of distinct unknowns

--- src/lin_sys/unknowns.rs
+++ src/lin_sys/unknowns.rs
@@ -1,4 +1,4 @@
-use core::{fmt::Error, marker::PhantomData};
+use core::{fmt::Error, marker::PhantomData, any::type_name};
 
 use nalgebra::Dim;
 
@@ -45,6 +45,13 @@ pub struct SingleLetterBoldfaceVecOfUnknowns<L, const N: NumberingTy> {
     phantom: PhantomData<()>,
 }
 
+pub struct SingleLetterBoldfaceVecOfDistinctSingleLetterUnknowns<const N: usize> {
+    pub c: char,
+    pub unknowns: [char; N],
+    // the private field forbids the usage of "default contructor"
+    phantom: PhantomData<()>,
+}
+
 impl<L, const N: NumberingTy> SingleLetterBoldfaceVecOfUnknowns<L, N> {
     #[cfg_attr(not(feature = "adt_const_params"), allow(non_upper_case_globals))]
     pub fn new(c: char, len: L) -> Self {
@@ -53,6 +60,29 @@ impl<L, const N: NumberingTy> SingleLetterBoldfaceVecOfUnknowns<L, N> {
         Self {
             c,
             len,
+            phantom: PhantomData::<()>,
+        }
+    }
+}
+
+impl<const N: usize> SingleLetterBoldfaceVecOfDistinctSingleLetterUnknowns<N> {
+    pub fn new(c: char, unknowns: [char; N]) -> Self {
+        #[cfg(debug_assertions)]
+        {
+            let mut hs = std::collections::HashSet::<char>::with_capacity(N);
+            for c in unknowns {
+                let is_newly_inserted = hs.insert(c);
+                if !is_newly_inserted {
+                    panic!("Unexpected repetitions. Unknowns in {type_name} should be distinct.",
+                        type_name = type_name::<Self>()
+                    )
+                }
+            }
+        };
+
+        Self {
+            c,
+            unknowns,
             phantom: PhantomData::<()>,
         }
     }
@@ -108,5 +138,43 @@ where
                 _ => panic!("unsupported numbering"),
             }
         ))
+    }
+}
+
+impl<const N: usize> Unknowns for SingleLetterBoldfaceVecOfDistinctSingleLetterUnknowns<N> {
+    fn is_empty(&self) -> bool {
+        self.unknowns.is_empty()
+    }
+
+    fn len(&self) -> Result<usize, OutOfBoundsError> {
+        Ok(self.unknowns.len())
+    }
+    
+    fn write<W, M>(&self, w: &mut W) -> Result<(), core::fmt::Error>
+    where
+        M: MathLatexMode,
+        W: LatexWriter<Mode = M>,
+    {
+        unsafe {
+            w.write_str(r"\textbf{")?;
+            w.write_char(self.c)?;
+            w.write_str(r"}")
+        }
+    }
+
+    fn validate_idx(&self, zbi: usize) -> Result<(), OutOfBoundsError> {
+        if zbi < self.unknowns.len() {
+            Ok(())
+        } else {
+            Err(OutOfBoundsError)
+        }
+    }
+
+    unsafe fn write_ith_unchecked<W, M>(&self, w: &mut W, zbi: usize) -> Result<(), Error>
+        where
+            M: MathLatexMode,
+            W: LatexWriter<Mode = M> {
+        let unknown_ref = self.unknowns.get_unchecked(zbi);
+        w.write_char(*unknown_ref)
     }
 }
